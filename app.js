e3d50a386f030b8f6a3212887386d8ddddffec3e
Gagandeep Singh <809gagan@gmail.com>
2021-07-21 21:26:00 +0530

    make it better

--- app.js
+++ app.js
@@ -5,6 +5,23 @@ const bodyParser = require('body-parser');
 const auth_route = require('./Routes/auth')
 const databaseINIT = require('./initDatabase');
 
+
+// Handling CORS 
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header(
+        'Access-Control-Allow-Headers',
+        'Content-Type, Origin , Accept , Authorization , X-Requested-With'
+    );
+
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT,POST,PATCH,DELETE,GET');
+        return res.status(200).json({});
+    }
+    next();
+})
+
 app.use(express.json())
 app.use(bodyParser.json());
 
