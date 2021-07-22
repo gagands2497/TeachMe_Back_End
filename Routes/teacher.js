62429d679c8f912cc87b8666b00dac00b3a008e0
Gagandeep Singh <809gagan@gmail.com>
2021-07-22 21:53:57 +0530

    make it better

--- Routes/teacher.js
+++ Routes/teacher.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const teacherController = require('../Controller/teacher-Controller');
-const Middleware = require('../Middleware/is-auth');
+const Middleware = require('../Middleware/Auth');
 
 router.get('/teacher_personal_profile', Middleware.isAuth , teacherController.teacher_personal_profile);
 
