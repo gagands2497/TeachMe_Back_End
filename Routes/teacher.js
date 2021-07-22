f7b5b223220507aa3f846ee557fa60d791d99cec
Gagandeep Singh <809gagan@gmail.com>
2021-07-22 21:31:10 +0530

    make it better

--- Routes/teacher.js
+++ Routes/teacher.js
@@ -4,9 +4,9 @@ const { body } = require('express-validator');
 const Student = require('../Models/Student');
 const Teacher = require('../Models/Teacher');
 const teacherController = require('../Controller/teacher');
-const { isAuth } = require('../Middleware/is-auth');
+const Middleware = require('../Middleware/is-auth');
 
-router.get('/teacher_personal_profile', isAuth, teacherController.teacher_personal_profile);
+router.get('/teacher_personal_profile', Middleware.isAuth, teacherController.teacher_personal_profile);
 
 
 module.exports = router;
